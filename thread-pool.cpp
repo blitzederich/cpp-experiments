a66a867a7a76f0093bdd17de5733646d4462d249
Alexander Samorodov <blitzederich@gmail.com>
2023-01-10 01:56:01 +0300

    base

---
+++ thread-pool.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <functional>
+#include <future>
+
+class ThreadPool {
+public:
+    ThreadPool(int workersCount) {
+        this->workers.reserve(workersCount);
+
+        while (workersCount--) {
+            auto worker = std::thread([&](){
+                this->loop();
+            });
+            this->workers.push_back(std::move(worker));
+        }
+    }
+
+    void AddTask(std::function<void()> task) {
+        auto lock = std::lock_guard(this->mu);
+        this->tasks.push(task);
+    }
+
+    template <typename T>
+    std::future<T> Async(std::function<T()> task) {
+        auto promise = std::make_shared<std::promise<T>>();
+        std::future<T> future = promise->get_future();
+
+        this->AddTask([promise, t = std::move(task)]() {
+            try {
+                auto res = t();
+                promise->set_value(std::move(res));
+            } catch (...) {
+                promise->set_exception(std::current_exception());
+            }
+        });
+
+        return future;
+    }
+
+    void Await() {
+        this->isEnding = true;
+
+        for (auto& w: this->workers) {
+            w.join();
+        }
+    }
+
+private:
+    bool isEnding = false;
+    std::mutex mu;
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+
+    void loop() {
+        for (;;) {
+            if (!this->mu.try_lock()) {
+                continue;
+            }
+
+            if (this->isEnding && this->tasks.empty()) {
+                this->mu.unlock();
+                return;
+            }
+
+            if (this->tasks.empty()) {
+                this->mu.unlock();
+                continue;
+            }
+
+            auto task = this->tasks.front();
+            this->tasks.pop();
+            mu.unlock();
+
+            task();
+        }
+    }
+};
+
+int main() {
+    auto pool = new ThreadPool(8);
+
+    for (;;) {
+        auto sum = pool->Async<int>([]() {
+            return 34 + 19;
+        });
+    }
+
+    pool->Await();
+}
